75cff3c1b3ec6313f85699dfe13e018ecc54f089
Lorenz Schori <lo@znerol.ch>
2019-07-13 20:30:56 +0200

    chore(ci): Fix copy-paste accident in test script

--- integration-test/test/test-dehydrated.sh
+++ integration-test/test/test-dehydrated.sh
@@ -8,4 +8,4 @@ git gau-ac \
 git gau-xargs -I"{WORKDIR}" \
 certhub-message-format "${CERTHUB_CERT_PATH}" x509 \
 certhub-dehydrated-run "${CERTHUB_CERT_PATH}" "${CERTHUB_CSR_PATH}" \
-certbot ${CERTHUB_DEHYDRATED_ARGS} --config "${CERTHUB_DEHYDRATED_CONFIG}"
+dehydrated ${CERTHUB_DEHYDRATED_ARGS} --config "${CERTHUB_DEHYDRATED_CONFIG}"
