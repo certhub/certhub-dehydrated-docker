b7257711e04a718385f534fe3e200245ac992310
Lorenz Schori <lo@znerol.ch>
2019-07-13 20:22:27 +0200

    chore(ci): Add automated tests and releases

---
+++ integration-test/test/config-setup-dehydrated.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+set -u
+set -x
+
+CONFIG_CSR_DIR=$(dirname "${CONFIG_CSR_PATH}")
+mkdir -p "${CONFIG_CSR_DIR}"
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:P-256 | \
+    openssl req -new -subj "/CN=${CONFIG_CERT_CN}/" -key - -out "${CONFIG_CSR_PATH}"
+
+CONFIG_DEHYDRATED_CONF_DIR=$(dirname "${CONFIG_DEHYDRATED_CONF_PATH}")
+mkdir -p "${CONFIG_DEHYDRATED_CONF_DIR}"
+
+cat <<EOF > "${CONFIG_DEHYDRATED_CONF_PATH}"
+CA="https://acme-staging-v02.api.letsencrypt.org/directory"
+CHALLENGETYPE="dns-01"
+BASEDIR=/etc/dehydrated
+HOOK=/usr/local/lib/certhub/dehydrated-hooks/lexicon-auth
+CURL_OPTS="-x tor:9050"
+IP_VERSION="4"
+EOF
---
+++ integration-test/test/test-dehydrated.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+set -u
+set -x
+
+git gau-ac \
+git gau-xargs -I"{WORKDIR}" \
+certhub-message-format "${CERTHUB_CERT_PATH}" x509 \
+certhub-dehydrated-run "${CERTHUB_CERT_PATH}" "${CERTHUB_CSR_PATH}" \
+certbot ${CERTHUB_DEHYDRATED_ARGS} --config "${CERTHUB_DEHYDRATED_CONFIG}"
